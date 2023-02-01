0eb9ce8824f8dcf0d0b4ae816ef9bce2319b771b
kdrai007 <987kundanrai@gmail.com>
2023-02-01 23:18:57 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import react from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  const [widgets, setWidgets] = useState<string[]>([]);
+  function handleOnDrag(e: React.DragEvent, widgetType: string) {
+    e.dataTransfer.setData("widgetType", widgetType);
+  }
+  function handleOnDrop(e: React.DragEvent) {
+    const widgetType = e.dataTransfer.getData("widgetType") as string;
+    console.log("widgetType", widgetType);
+    setWidgets([...widgets, widgetType]);
+  }
+  function handleDragOver(e: React.DragEvent) {
+    e.preventDefault();
+  }
   return (
-    <div className="container">
-      drag and drop
+    <div className="app">
+      <div className="widgets">
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget A")}>
+          Widget A
+        </div>
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget B")}>
+          Widget B
+        </div>
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget C")}>
+          Widget C
+        </div>
+      </div>
+      <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
+        {widgets.map((widget, index) => {
+          return <div className='widget' key={index}>
+            {widget}
+          </div>
+        })}
+      </div>
     </div>
   )
 }
