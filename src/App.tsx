30b226b783483eaa5c6c140a6281d7677482d02b
kdrai007 <987kundanrai@gmail.com>
2023-02-01 22:56:37 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      drag and drop
+    </div>
+  )
+}
+
+export default App
